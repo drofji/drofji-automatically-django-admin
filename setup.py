381d7c307b4c322da110c6b9ab31f1739ba92d18
Administrator <sanowese@gmail.com>
2025-11-17 09:43:30 +0100

    Read Me

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ setup(
     version="1.0.0",
     packages=find_packages(),
     include_package_data=True,
+    python_requires=">=3.10",
+
     install_requires=[
-        "Django>=4.2",
+        "Django>=5.2",
         "django-admin-rangefilter~=0.13.3",
         "django-admin-interface~=0.28.9",
         "django-colorfield~=0.11.0",
@@ -14,9 +16,12 @@ setup(
         "django-crontab~=0.7.1",
         "django-datepick~=0.1.2",
     ],
+
     classifiers=[
         "Framework :: Django",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
     ],
 )
